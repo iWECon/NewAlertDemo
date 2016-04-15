bb96facea5caa16e584457abb6f9b01dcc357ff6
iWECon <i.1214@yahoo.com>
2016-04-15 11:56:17 +0800

    Initial Commit

---
+++ NewAlertDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewAlertDemo
+//
+//  Created by iWe on 16/4/15.
+//  Copyright © 2016年 iWe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
